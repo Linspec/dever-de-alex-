cc9670656a29555865342d8813ddafd1e2588cd4
Linspec <43051963+Linspec@users.noreply.github.com>
2018-11-21 19:38:13 -0200

    #fé

---
+++ dever 7.cs
@@ -0,0 +1,14 @@
+﻿}
+printf("\n\n");
+// Obtenção da resposta do aluno
+// Obtenção da resposta do aluno
+printf("Aluno %d\n", a+1);
+for (i = 0; i< 10; i++){
+    printf("A resposta da questao %d: ", i+1);
+scanf("%s", &resposta[i]);   
+}
+ // Impressão de tabela
+// Impressão de tabela
+printf("\n\nQuestao\tRespostaGabaritoSituacao\n");
+for (i = 0; i< 10; i++){
+    if (resposta[i] == gabarito[i]){
